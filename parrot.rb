494b34b577e33cb8aa1966a9f52bd0e48aa6d2a0
Emily Welsch <emwelsch@gmail.com>
2018-02-21 19:34:46 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-# phrase = "Squawk!"
 def parrot(phrase = "Squawk!")
-puts "Squawk!"
-return "Squawk!"
+puts phrase
+return phrase
 end
